048f5571f620e153e4f67a6e7d0291176bb6d858
Andrew <Andy>
2023-08-04 22:40:13 +0300

    DZ2

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите первое число ");
+int firstnumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int secondnumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число ");
+int thirdnumber = Convert.ToInt32(Console.ReadLine());
+if (firstnumber > secondnumber && firstnumber > thirdnumber)
+{
+Console.Write("max = " + firstnumber);
+}
+else if (secondnumber > firstnumber && secondnumber > thirdnumber)
+{
+Console.Write("max = " + secondnumber);
+}
+else if (thirdnumber > secondnumber && thirdnumber > firstnumber)
+{
+Console.Write("max = " + thirdnumber);
+}
+else
+{Console.Write("Числа равны");}
